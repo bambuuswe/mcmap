3b334b05368c243c64d938b41bbd4956f951f402
Bambuu <hpelle966@gmail.com>
2026-02-01 18:16:48 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,42 +1,38 @@
 const map = L.map("map", {
   zoomControl: false,
   attributionControl: false,
-  zoomSnap: 1
+  minZoom: 16,
+  maxZoom: 16
 }).setView([0, 0], 16);
 
-// OpenStreetMap tiles (pixel-art look via CSS)
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19
-}).addTo(map);
+/*
+  ↓↓↓ VIKTIG DEL ↓↓↓
+  Carto Voyager ger tydliga gröna block
+  som CSS sen gör fyrkantiga
+*/
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png",
+  { maxZoom: 19 }
+).addTo(map);
 
-// Spelare-markör
-const playerIcon = L.divIcon({
-  className: "player",
-  html: "🟩",
-  iconSize: [24, 24],
-  iconAnchor: [12, 12]
+// PIXLIG VIT DIAMANT (position)
+const playerIcon = L.icon({
+  iconUrl: "player.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 16]
 });
 
-let playerMarker = L.marker([0, 0], { icon: playerIcon }).addTo(map);
+const player = L.marker([0, 0], {
+  icon: playerIcon
+}).addTo(map);
 
-// Geolocation
-if ("geolocation" in navigator) {
-  navigator.geolocation.watchPosition(
-    (pos) => {
-      const lat = pos.coords.latitude;
-      const lon = pos.coords.longitude;
-
-      playerMarker.setLatLng([lat, lon]);
-      map.setView([lat, lon], 16);
-    },
-    (err) => {
-      console.error(err);
-      alert("Kunde inte hämta position 😢");
-    },
-    {
-      enableHighAccuracy: true
-    }
-  );
-} else {
-  alert("Geolocation stöds inte i din browser");
-}
+// GPS
+navigator.geolocation.watchPosition(
+  pos => {
+    const { latitude, longitude } = pos.coords;
+    player.setLatLng([latitude, longitude]);
+    map.setView([latitude, longitude]);
+  },
+  err => alert("GPS error"),
+  { enableHighAccuracy: true }
+);
