cf9803599a9d7e0a983d228989792ea0ae7827cf
Bambuu <hpelle966@gmail.com>
2026-02-01 18:20:32 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,37 +1,32 @@
 const map = L.map("map", {
   zoomControl: false,
   attributionControl: false,
-  minZoom: 16,
-  maxZoom: 16
-}).setView([0, 0], 16);
+  minZoom: 17,
+  maxZoom: 17
+}).setView([0, 0], 17);
 
-/*
-  ↓↓↓ VIKTIG DEL ↓↓↓
-  Carto Voyager ger tydliga gröna block
-  som CSS sen gör fyrkantiga
-*/
+// PLATT karta som pixeliseras snyggt
 L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png",
+  "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
   { maxZoom: 19 }
 ).addTo(map);
 
-// PIXLIG VIT DIAMANT (position)
+// PIXLIG VIT DIAMANT (player.png)
 const playerIcon = L.icon({
   iconUrl: "player.png",
   iconSize: [32, 32],
   iconAnchor: [16, 16]
 });
 
-const player = L.marker([0, 0], {
-  icon: playerIcon
-}).addTo(map);
+const player = L.marker([0, 0], { icon: playerIcon }).addTo(map);
 
 // GPS
 navigator.geolocation.watchPosition(
   pos => {
-    const { latitude, longitude } = pos.coords;
-    player.setLatLng([latitude, longitude]);
-    map.setView([latitude, longitude]);
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+    player.setLatLng([lat, lon]);
+    map.setView([lat, lon]);
   },
   err => alert("GPS error"),
   { enableHighAccuracy: true }
